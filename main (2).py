4b4cd40d53ce04afcc10c84fd0319bd348163578
nadilah07 <147677720+nadilah07@users.noreply.github.com>
2024-04-26 10:20:26 +0800

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,14 @@
+# variasi dengan tipe data integer 
+integra_var = 10
+
+# variasi dengan tipe data string 
+string_var = "hello, world!"
+
+# variasi dengan tipe data boolean
+boolean_var = True
+
+# menampilkan nilai dri masing masing variabel 
+print("nilai integer_var.", integra_var)
+print("nilai flot_var.", flot_var) 
+print("nilai string_var.", string_var)
+print("nilai boolean_var.", boolean_var)
