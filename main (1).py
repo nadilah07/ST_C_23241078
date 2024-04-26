760efcd82319a26c6991e7eda31cbd0cb07f70ee
nadilah07 <147677720+nadilah07@users.noreply.github.com>
2024-04-26 10:19:55 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+# ini pelajaran variabel 
+e = 9
+print(e)
+print("nilai saya adalah = " , e)
+z = 7
+print (z)
+print ("nilai saya adalah = " , z)
+e = z
+print ("nilai baru saya adalah = " , e)
+"
