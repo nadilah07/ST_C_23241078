9ecd4b1ea6a336aeff069b6033280045cb6e6cde
nadilah07 <147677720+nadilah07@users.noreply.github.com>
2024-06-27 14:46:15 +0800

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,17 @@
+#Belajar mulit percabangan 
+
+nama = input("Masukkan nama : ")
+if nama == "efy": 
+  print("dosen ganteng")
+elif nama =="dila":
+  print("mahasiswa cantik")
+elif nama =="putra":
+  print("mahasiswa ngak ganteng") # aksi ture 3 
+else:
+  print("andi bukan siapa-siapa") # false 
+  print("program selesai") # aksi end
+  
+
+
+  
+  
