f6efc19b248562a42a4301184b1ac609f840ef7a
nadilah07 <147677720+nadilah07@users.noreply.github.com>
2024-06-27 14:42:37 +0800

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,44 @@
+# tipe data integers, float, string, boolean
+a = 12
+z = 12.5
+d = "12"
+c ="ini tipe data"
+w = True
+
+print ("string")
+#sting 
+print (c, "/n")
+
+print("integer")
+#integer
+#penjumlahan Bilangan 
+penjumlahan = a + z 
+print("penjumlahan a + z =", penjumlahan)
+#pengurangan Bilangan
+pengurangan = a - z
+print("pengurangan a - z =", pengurangan)
+#perkalian Bilangan
+perkalian = a * z
+print("perkalian a * z =", perkalian)
+#pembagian Bilangan
+pembagian = a / z
+print("pembagian a / z =", pembagian, "/n")
+
+print("float")
+#float
+#penjumlahan Bilangan
+penjumlahan = a + z
+print("penjumlahan d + z =", penjumlahan)
+#pengurangan Bilangan
+pengurangan = a - z
+print("pengurangan d - z =", pengurangan)
+#perkalian Bilangan
+perkalian = a * z
+print("perkalian d * z =", perkalian)
+#pembagian Bilangan
+pembagian = a / z
+print("pembagian d / z =", pembagian, "/n")
+
+print("boolean")
+print("w =", w)
+print("w =", not w)
